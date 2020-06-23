b5cb862142798e136f23b990e7a934f2f3a45b74
macarenaferrero <maquitis1415@gmail.com>
2020-06-22 22:28:15 -0300

    TP4 Terminado

---
+++ TP4_linkedlist/src/main.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#include "utn.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+
+int main()
+{
+    int option = 0;
+    int archivoCargado = 0;
+    Employee* auxEmployee;
+    int auxIndice;
+    int contadorAux;
+    LinkedList* sublista;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    do{
+        	utn_getNumero(&option,"1. Cargar los datos de los empleados desde el archivo data.csv (modo texto)\n"
+        						  "2. Cargar los datos de los empleados desde el archivo data.csv (modo binario)\n"
+        						  "3. Alta de empleado\n"
+        						  "4. Modificar datos de empleado\n"
+        						  "5. Baja de empleado\n"
+        						  "6. Listar empleados\n"
+        						  "7. Ordenar empleados por nombre\n"
+        						  "8. Guardar los datos de los empleados en el archivo data.csv (modo texto)\n"
+        						  "9. Guardar los datos de los empleados en el archivo data.csv (modo binario)\n"
+        						  "10. Ordenar empleados por ID\n"
+        						  "11. Buscar empleado contenido\n"
+        						  "12. Clonar lista\n"
+        						  "13. Crear subista de los primeros 10 empleados\n"
+        						  "14. Corroborar sublista\n"
+        						  "15. Filtrar\n"
+        						  "16. Count\n"
+        						  "17. Salir\n",
+    							  "Opción inválida\n",
+    							  1,17,2);
+            switch(option)
+            {
+                case 1:
+                	if(archivoCargado == 0)
+                	{
+						if(!controller_loadFromText("data.csv",listaEmpleados))
+						{
+							archivoCargado = 1;
+						}
+                	}
+                	else
+                	{
+                		printf("Archivo ya cargado\n");
+                	}
+                    break;
+                case 2:
+                	if(archivoCargado == 0)
+                	{
+						controller_loadFromBinary("data.dat",listaEmpleados);
+						archivoCargado = 1;
+                    }
+                	else
+					{
+						printf("Archivo ya cargado\n");
+					}
+                    break;
+                case 3:
+                	controller_addEmployee(listaEmpleados);
+                    break;
+                case 4:
+                	controller_editEmployee(listaEmpleados);
+                    break;
+                case 5:
+                	controller_removeEmployee(listaEmpleados);
+                    break;
+                case 6:
+                	controller_ListEmployee(listaEmpleados);
+                    break;
+                case 7:
+                	controller_sortEmployee(listaEmpleados);
+                    break;
+                case 8:
+                	controller_saveAsText("data.csv",listaEmpleados);
+                    break;
+                case 9:
+                	controller_saveAsBinary("data.dat",listaEmpleados);
+                    break;
+                case 10:
+                	controller_sortEmployeebyID(listaEmpleados);
+                	break;
+                case 11:
+                	auxEmployee = ll_get(listaEmpleados,1);
+                	controller_containsElemento(listaEmpleados,auxEmployee);
+					auxIndice = ll_indexOf(listaEmpleados,auxEmployee);
+					printf("El empleado se encuentra en el indice %d\n", auxIndice);
+                	break;
+                case 12:
+                	if(ll_clone(listaEmpleados) != NULL)
+                	{
+                		printf("Lista clonada\n");
+                		sublista = ll_clone(listaEmpleados);
+                		ll_map(sublista,employee_imprimirGral);
+                	}
+                	break;
+                case 13:
+                	if(ll_subList(listaEmpleados,0,10) != NULL)
+                	{
+                		printf("Sublista creada\n");
+                		sublista = ll_subList(listaEmpleados,0,10);
+                		ll_map(sublista,employee_imprimirGral);
+                	}
+                	break;
+                case 14:
+                	if(ll_containsAll(listaEmpleados,sublista)==1)
+                	{
+                		printf("Los elementos de sublista están contenidos\n");
+                	}
+                	else if(ll_containsAll(listaEmpleados,sublista)==0)
+                	{
+                		printf("Al menos un elemento no está contenido\n");
+                	}
+                	else
+                	{
+                		printf("La lista es NULL\n");
+                	}
+                	break;
+                case 15:
+                	sublista = ll_filter(listaEmpleados,calcularEmpleadosHsMayoresA);
+                	if(sublista != NULL)
+                	{
+                		ll_map(sublista,employee_imprimirGral);
+                		printf("Lista filtrada\n");
+                	}
+                	break;
+                case 16:
+                	contadorAux = ll_count(listaEmpleados,calcularEmpleadosHsMayoresA);
+                	printf("Hay %d empleados que superan las 300 hs. trabajadas\n",contadorAux);
+                	break;
+
+            }
+        }while(option != 17);
+    return 0;
+}
+
